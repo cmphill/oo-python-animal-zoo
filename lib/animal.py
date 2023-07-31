ea476a9a6f263cc0985f1cf31c500f274f1b3f4a
Cooper <cmphill@pm.me>
2023-07-31 14:44:38 -0600

    passing the challenges

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,55 @@
+# from zoo import Zoo
 class Animal:
-    pass
+    all_animal = []
+    def __init__(self, species, weight, nickname, zoo):
+        self.species = species
+        self.weight = weight
+        self.nickname = nickname
+        self.zoo = zoo
+        Animal.all_animal.append(self)
+    # def nickname(self):
+    #     return self.nickname
+    # def weight(self):
+    #     return self.weight
+    # def species(self):
+    #     return self.species
+
+    # def zoo(self):
+    #     return self.zoo
+    @property
+    def species(self):
+        return self._species
+    @species.setter
+    def species(self, value):
+        if isinstance(value, str):
+            self._species = value
+    @property
+    def nickname(self):
+        return self._nickname
+    @nickname.setter
+    def nickname(self, value):
+        if isinstance(value, str):
+            self._nickname = value
+    @property
+    def weight(self):
+        return self._weight
+    @weight.setter
+    def weight(self, value):
+        if isinstance(value, int):
+            self._weight = value
+    # @property
+    # def zoo(self):
+    #     return self._zoo
+    # @zoo.setter
+    # def zoo(self, value):
+    #     if isinstance(value, Zoo):
+    #         self._zoo = value
+            
+    @classmethod
+    def find_by_species(cls, speciesCheck):
+        return [animal for animal in cls.all_animal if animal.species == speciesCheck]
+    
+if __name__ == "__main__":
+    import ipdb
+    ipdb.set_trace()
+
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,67 @@
-from lib.animal import Animal
+
 
 class Zoo:
-    pass
+    zoo_all = []
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        Zoo.zoo_all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,value):
+        if isinstance(value, str):
+            self._name = value
+    @property
+    def location(self):
+        return self._location
+    @location.setter
+    def location(self,value):
+        if isinstance(value, str):
+            self._location = value
+    def animal_species(self):
+        # for animal in Animal.all_animal:
+        #     if animal.zoo == self.name:
+        #         return animal.species
+        return set(animal.species for animal in Animal.all_animal if animal.zoo == self.name)
+    
+    def find_by_species(self,target):
+        res = []
+        for animal in Animal.all_animal:
+            if animal.zoo == self.name:
+                if animal.species == target:
+                    res.append(animal)
+        return res
+    def animal_nicknames(self):
+        res = []
+        for animal in Animal.all_animal:
+            if animal.zoo == self.name:
+                res.append(animal.nickname)
+        return res
+    @classmethod
+    def find_by_location(cls, target):
+        res = []
+        for zoo in cls.zoo_all:
+            if zoo.location == target:
+                res.append(zoo)
+        return res
+
+
+if __name__ == "__main__":
+    import ipdb
+    from animal import Animal
+    zoo1 =Zoo("Denver Zoo", "Denver")
+    zoo2 = Zoo("LA Zoo", "LA")
+    zoo3 = Zoo("Flatiron Zoo", "Denver")
+    animal1 = Animal("dog", 10, "doggo", "LA Zoo")
+    animal2 = Animal("Cat", 10, "catto", "Denver Zoo")
+    animal3 = Animal("Cat", 10, "kitty", "LA Zoo")
+    animal4 = Animal("Cat", 10, "kitten", "Denver Zoo")
+    animal5 = Animal("Sheep", 10, "sheepdog", "Denver Zoo")
+    animal6 = Animal("dog", 10, "pupper", "Denver Zoo")
+    ipdb.set_trace()
+    
+        
+    
